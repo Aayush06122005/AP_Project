b37d656d81d18a97fd13f41951a42a8cee3f9cdb
Sushant-4444 <sushant23537@iiitd.ac.in>
2024-11-23 15:58:04 +0530

    added box2d bodies

--- core/src/main/java/io/github/game/MaterialsPackage/Block.java
+++ core/src/main/java/io/github/game/MaterialsPackage/Block.java
@@ -1,6 +1,11 @@
 package io.github.game.MaterialsPackage;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 import io.github.game.Mygame;
 
 public abstract class Block {
@@ -10,14 +15,26 @@ public abstract class Block {
     private float positionX;
     private float positionY;
     private Mygame gameInstance;
+    private Body body;
 
-    public Block(float a, float b, float c, float d, Mygame e){
+
+    public Block(float a, float b, float c, float d, Mygame e,World world){
         positionX = a;
         positionY = b;
         length_of_x = c;
         length_of_y = d;
         gameInstance = e;
         imgToShow = new Texture("space.jpg");
+
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.StaticBody;
+        bodyDef.position.set(a, b);
+        body = world.createBody(bodyDef);
+
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(c / 2, d / 2);
+        body.createFixture(shape, 0.0f);
+        shape.dispose();
     }
 
     public void setImg(Texture aa){
@@ -25,6 +42,7 @@ public abstract class Block {
     }
 
     public void addToScreen(){
+        Vector2 position = body.getPosition();
         gameInstance.ourSpriteBatch.draw(imgToShow,positionX ,positionY ,length_of_x,length_of_y);
     }
     public void disposeFromScreen(){
--- core/src/main/java/io/github/game/MaterialsPackage/SolidTriangleBlock.java
+++ core/src/main/java/io/github/game/MaterialsPackage/SolidTriangleBlock.java
@@ -1,11 +1,12 @@
 package io.github.game.MaterialsPackage;
+import com.badlogic.gdx.physics.box2d.World;
 
 import com.badlogic.gdx.graphics.Texture;
 import io.github.game.Mygame;
 
 public class SolidTriangleBlock extends Block {
-    public SolidTriangleBlock(String material, float a, float b, float c, float d, Mygame e) {
-        super(a, b, c, d, e);
+    public SolidTriangleBlock(String material, float a, float b, float c, float d, Mygame e,World world) {
+        super(a, b, c, d, e,world);
         if (material.equals("wooden")) {
             this.setImg(new Texture("solidTriangleWood.png"));
         } else if (material.equals("Steel")) {
